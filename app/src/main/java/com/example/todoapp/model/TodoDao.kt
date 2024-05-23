92fd394189092f15a92512a5369d7c6c98f43261
hilmyyes <irfansahilmy@gmail.com>
2024-05-23 16:46:44 +0700

    Base Materi Week 9
    Semua dah bisa dan sesuai base di week 8 & 9

---
+++ app/src/main/java/com/example/todoapp/model/TodoDao.kt
@@ -0,0 +1,27 @@
+package com.example.todoapp.model
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg todo:Todo)
+
+    @Query("SELECT * FROM todo ORDER BY priority DESC")
+    fun selectAllTodo(): List<Todo>
+
+    @Query("SELECT * FROM todo WHERE uuid= :id")
+    fun selectTodo(id:Int): Todo
+
+    @Delete
+    fun deleteTodo(todo:Todo)
+
+    @Query("UPDATE todo SET title=:title, notes=:notes, priority=:priority WHERE uuid = :id")
+    fun update(title:String, notes:String, priority:Int, id:Int)
+
+
+}
---
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.todoapp.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.databinding.TodoItemLayoutBinding
+import com.example.todoapp.model.Todo
+
+class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) -> Unit)
+    : RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
+    class TodoViewHolder(var binding: TodoItemLayoutBinding):
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        var binding = TodoItemLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent,false)
+        return TodoViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.binding.checkTask.text = todoList[position].title
+
+        holder.binding.imgEdit.setOnClickListener {
+            val action =
+                TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
+
+            Navigation.findNavController(it).navigate(action)
+        }
+
+
+
+        holder.binding.checkTask.setOnCheckedChangeListener {
+                compoundButton, b ->  if(compoundButton.isPressed) {
+            adapterOnClick(todoList[position])
+        }
+
+        }
+
+    }
+
+
+
+    fun updateTodoList(newTodoList: List<Todo>) {
+        todoList.clear()
+        todoList.addAll(newTodoList)
+        notifyDataSetChanged()
+    }
+
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+
+}
+
+
+
+
+
+

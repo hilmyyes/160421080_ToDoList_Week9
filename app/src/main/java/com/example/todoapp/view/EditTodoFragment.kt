fcc3a46eeb63e183252fd22fd5c9560d5719f929
hilmyyes <irfansahilmy@gmail.com>
2024-05-25 14:27:42 +0700

    week 10 - base
    done week 10, all normal

--- app/src/main/java/com/example/todoapp/view/EditTodoFragment.kt
+++ app/src/main/java/com/example/todoapp/view/EditTodoFragment.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp.view
 
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,22 +13,28 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentCreateTodoBinding
+import com.example.todoapp.databinding.FragmentEditTodoBinding
+import com.example.todoapp.model.Todo
 import com.example.todoapp.viewmodel.DetailTodoViewModel
 
 
-class EditTodoFragment : Fragment() {
-    private lateinit var binding: FragmentCreateTodoBinding
+class EditTodoFragment : Fragment(), RadioClick, TodoSaveChangesClick {
+    private lateinit var binding: FragmentEditTodoBinding
     private lateinit var viewModel: DetailTodoViewModel
+    private lateinit var dataBinding: FragmentEditTodoBinding
 
     override fun onCreateView(inflater: LayoutInflater, container:
     ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentCreateTodoBinding.inflate(
-            inflater, container,
-            false
-        )
+        binding = FragmentEditTodoBinding.inflate(inflater,container, false)
+
         return binding.root
 
     }
+
+    override fun onRadioClick(v: View, priority: Int, obj: Todo) {
+        obj.priority = priority
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,33 +45,43 @@ class EditTodoFragment : Fragment() {
 
         binding.txtJudulTodo.text = "Edit Todo"
         binding.btnAdd.text = "Save Changes"
+        binding.radioListener = this
+        binding.saveListener = this
 
-        binding.btnAdd.setOnClickListener {
-            val radio =
-                view.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
 
-            viewModel.update(binding.txtTitle.text.toString(), binding.txtNotes.text.toString(),
-                radio.tag.toString().toInt(), uuid)
-            Toast.makeText(view.context, "Todo updated", Toast.LENGTH_SHORT).show()
-            Navigation.findNavController(it).popBackStack()
-        }
+//        binding.btnAdd.setOnClickListener {
+//            val radio =
+//                view.findViewById<RadioButton>(binding.radioGroupPriority.checkedRadioButtonId)
+//
+//            viewModel.update(binding.txtTitle.text.toString(), binding.txtNotes.text.toString(),
+//                radio.tag.toString().toInt(), uuid)
+//            Toast.makeText(view.context, "Todo updated", Toast.LENGTH_SHORT).show()
+//            Navigation.findNavController(it).popBackStack()
+//        }
 
         observeViewModel()
 
 
 
     }
+    override fun onTodoSaveChangesClick(v: View, obj: Todo) {
+        viewModel.update(obj.title, obj.notes, obj.priority, obj.uuid)
+        Toast.makeText(v.context, "Todo Updated", Toast.LENGTH_SHORT).show()
+    }
+
 
     fun observeViewModel() {
         viewModel.todoLD.observe(viewLifecycleOwner, Observer {
-            binding.txtTitle.setText(it.title)
-            binding.txtNotes.setText(it.notes)
+            binding.todo = it
 
-            when (it.priority) {
-                1 -> binding.radioLow.isChecked = true
-                2 -> binding.radioMedium.isChecked = true
-                else -> binding.radioHigh.isChecked = true
-            }
+//            binding.txtTitle.setText(it.title)
+//            binding.txtNotes.setText(it.notes)
+//
+//            when (it.priority) {
+//                1 -> binding.radioLow.isChecked = true
+//                2 -> binding.radioMedium.isChecked = true
+//                else -> binding.radioHigh.isChecked = true
+//            }
 
         })
     }
--- app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
+++ app/src/main/java/com/example/todoapp/view/TodoListAdapter.kt
@@ -1,14 +1,19 @@
 package com.example.todoapp.view
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.databinding.TodoItemLayoutBinding
 import com.example.todoapp.model.Todo
 
-class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) -> Unit)
-    : RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
+
+class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) -> Unit):
+    RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>(),
+        TodoCheckedChangeListener, TodoEditClick
+    {
     class TodoViewHolder(var binding: TodoItemLayoutBinding):
         RecyclerView.ViewHolder(binding.root)
 
@@ -20,29 +25,50 @@ class TodoListAdapter(val todoList:ArrayList<Todo>, val adapterOnClick : (Todo) 
 
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.binding.checkTask.text = todoList[position].title
-
-        holder.binding.imgEdit.setOnClickListener {
-            val action =
-                TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
-
-            Navigation.findNavController(it).navigate(action)
-        }
+        holder.binding.todo = todoList[position]
+        holder.binding.listener = this
+        holder.binding.editListener = this
 
 
-
-        holder.binding.checkTask.setOnCheckedChangeListener {
-                compoundButton, b ->  if(compoundButton.isPressed) {
-            adapterOnClick(todoList[position])
-        }
-
-        }
+//
+//        holder.binding.checkTask.text = todoList[position].title
+//
+//        holder.binding.imgEdit.setOnClickListener {
+//            val action =
+//                TodoListFragmentDirections.actionEditTodoFragment(todoList[position].uuid)
+//
+//            Navigation.findNavController(it).navigate(action)
+//
+//        }
+//
+//
+//
+//        holder.binding.checkTask.setOnCheckedChangeListener {
+//                compoundButton, b ->  if(compoundButton.isPressed) {
+//            adapterOnClick(todoList[position])
+//        }
+//
+//        }
 
     }
 
+        override fun onCheckedChanged(cb: CompoundButton,
+                                      isChecked: Boolean, obj: Todo) {
+            if(cb.isPressed) {
+                adapterOnClick(obj)
+            }
+        }
 
 
-    fun updateTodoList(newTodoList: List<Todo>) {
+        override fun onTodoEditClick(v: View) {
+            val uuid = v.tag.toString().toInt()
+            val action = TodoListFragmentDirections.actionEditTodoFragment(uuid)
+
+            Navigation.findNavController(v).navigate(action)
+        }
+
+
+        fun updateTodoList(newTodoList: List<Todo>) {
         todoList.clear()
         todoList.addAll(newTodoList)
         notifyDataSetChanged()
